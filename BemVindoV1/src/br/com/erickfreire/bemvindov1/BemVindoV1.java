54eb4875432e71f63d159d52e8bb14164fb03c7e
Erick Freire <erilefr@gmail.com>
2022-09-30 18:34:12 -0300

    Versao1
    Versao1

---
+++ BemVindoV1/src/br/com/erickfreire/bemvindov1/BemVindoV1.java
@@ -0,0 +1,14 @@
+package br.com.erickfreire.bemvindov1;
+
+/**
+ * Programa que exibem uma mensagem de boas vidas
+ * @author Erick Freire
+ * @version 1 - Criado em 30-09-2022
+ */
+
+public class BemVindoV1 {
+	public static void main(String[] args) {
+		System.out.println("Bem Vindo a Programação em Java!");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bem_Vindo_V1
  Bem Vindo A Programação Em Java!
+ Programa que exibem uma mensagem de boas vindas
+
+ ## Imagem do Programa
+![Currículo no Computador](Bem_Vindo_V1.png)
+
+## Video: 
+https://www.youtube.com/watch?v=oqjr3QJ5hrM
